f9ac9f5ba26aa6cb29099c2d8b0bf0ca64b22bec
Гость <seriousmail07@gmail.com>
2017-12-13 22:07:07 +0200

    20171213

--- PYTHON/ld1.py
+++ PYTHON/ld1.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
+
 a = input("Ievadi skaitli: ")
 print type(a)
 
-k = 1
-b = a + 10
-while a <= b:
-   c = a % k
-   print "%d   %d"%(a,c)
-   k = k + 1
-   c1 = a % k 
-   print "  %d"%(c1)
-   a = a + 1
+print ("\t%1\t%2\t%3\t%4\t%5\t%10")
 
+k = 1
+a1 = a + 10
+while a <= a1:
+ c = a % k
+ print "%d %d"%(a,c)
+ while k <=5:
+  k = k + 1
+ a = a + 1
---
+++ PYTHON/ld2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from math import sin
+# -*- coding: utf-8 -*-
+
+x = 1. * input("Ievadi argumentu (x): ")
+print type(x)
+
+k = 1
+a = -(x**3)/8
+S = a
+print "a1 = %6.2f\t S1 = %6.2f"%(a,S)
+
+while k <= 3:
+ k = k + 1
+ R = (x**2)/(k*(k+1))
+ S = S + a
+ print "a%d = %6.2f\t S%d = %6.2f"%(k,a,k,S)
