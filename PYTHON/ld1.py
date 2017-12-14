9cabe638d1d9f3105b871e1aac0f13d4874c8a07
aijatikuma@inbox.lv <seriousmail07@gmail.com>
2017-12-14 12:00:09 +0200

    20171214

--- PYTHON/ld1.py
+++ PYTHON/ld1.py
@@ -3,13 +3,18 @@
 a = input("Ievadi skaitli: ")
 print type(a)
 
-print ("\t%1\t%2\t%3\t%4\t%5\t%10")
+print ("\t%1\t%2\t%3\t%4\t%5")
 
-k = 1
 a1 = a + 10
+
+
 while a <= a1:
- c = a % k
- print "%d %d"%(a,c)
- while k <=5:
-  k = k + 1
+ k = 1
+ while k <= 5:
+  print "%d"%(a),
+  c = a % k
+  print "\t%d" %(c),
+  k = k + 1 
  a = a + 1
+ print "\n%d" %(a)
+ print
